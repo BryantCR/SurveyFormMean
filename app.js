8a77db4c762a92558e85ba5e638e05963ba55509
BryantCR <bryancvargas20@gmail.com>
2021-11-18 20:34:43 -0600

    Ok

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var session = require("express-session");
+var app = express();
+
+app.use(express.json()) //en el curriculum nos enseñan esta
+app.use(bodyParser.urlencoded({entended: true}));
+app.use(express.static(__dirname + "/static"));
+app.use(session({
+    secret: 'root',
+}))
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("home");
+})
+
+app.post("/result", function(req, res){
+    req.session.results = req.body;
+    console.log(req.body);
+    res.redirect("results");
+})
+
+app.get("/result", function(req, res){
+    res.render("results", {results:req.session.results});
+})
+
+app.listen(8080, function(){
+    console.log("Listening on port: 8080");
+})
